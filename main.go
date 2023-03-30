8bcd8d9feb94144f35143555b5082e453fed9e89
dubs3c <dubs3c@users.noreply.github.com>
2023-03-30 15:01:25 +0200

    allow user to specify files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -18,19 +19,37 @@ func Permutate(out chan<- string, domain *string, permutationList *[]string) {
 
 func main() {
 
-	permutationList := []string{"mail", "vpn", "admin", "www", "remote"}
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run permutate.go <file1> <file2>")
+		os.Exit(1)
+	}
+
+	filename1 := os.Args[1]
+	file1, err := os.Open(filename1)
+	if err != nil {
+		fmt.Println("Error: Failed to open the file1.")
+		os.Exit(1)
+	}
+	defer file1.Close()
+
+	filename2 := os.Args[2]
+	file2, err := os.Open(filename2)
+	if err != nil {
+		fmt.Println("Error: Failed to open the file2.")
+		os.Exit(1)
+	}
+	defer file2.Close()
+
+	permutationList := make([]string, 0)
+	scanner := bufio.NewScanner(file1)
+	for scanner.Scan() {
+		permutationList = append(permutationList, scanner.Text())
+	}
+
 	workers := 16
 	domains := make(chan string, workers)
 	output := make(chan string, workers)
 	wg := &sync.WaitGroup{}
-
-	file, err := os.OpenFile("data.txt", os.O_RDONLY, os.ModePerm)
-
-	if err != nil {
-		log.Fatal("Could not open domain file, error: ", err)
-	}
-
-	defer file.Close()
 
 	startTime := time.Now()
 
@@ -60,7 +79,7 @@ func main() {
 		}
 	}()
 
-	scanner := bufio.NewScanner(file)
+	scanner = bufio.NewScanner(file2)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
